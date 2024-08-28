ba81d177eae3c2949b6623f2e461599bf9acca83
Javier Santiago <li_s.javier@hotmail.com>
2024-08-28 13:16:30 -0600

    Add get max ID with multiple prefix without depending of length and can pass CONNECTION name in case of multi tenant

--- src/IdGenerator.php
+++ src/IdGenerator.php
@@ -1,27 +1,82 @@
-<?php
+<?php namespace Haruncpi\LaravelIdGenerator;
 
-namespace Haruncpi\LaravelIdGenerator;
+use Illuminate\Support\Facades\DB;
+use Exception;
 
-use Illuminate\Support\Facades\DB, Exception;
-
+/**
+ * IdGenerator Class
+ * @since 1.0.0
+ */
 class IdGenerator
 {
-
-    private function getFieldType($table, $field)
+    /**
+     * Get type of a field
+     *
+     * @param string $table table name
+     * @param string $field field name
+     *
+     * @throws Exception if field type return null
+     * @return array
+     *
+     * @since 1.0.0
+     */
+    private function getFieldType($table, $field, $connection = null)
     {
-        $colsType = DB::select('describe ' . $table);
+        if($connection == null){
+            $connection = config('database.default');
+        }
+        $driver = DB::connection($connection)->getDriverName();
+        $database = DB::connection($connection)->getDatabaseName();
+
+        if ($driver == 'mysql') {
+            $sql = 'SELECT column_name AS "column_name",data_type AS "data_type",column_type AS "column_type" ';
+            $sql .= 'FROM information_schema.columns ';
+            $sql .= 'WHERE table_schema=:database AND table_name=:table';
+        } else {
+            // column_type not available in postgres SQL
+            // table_catalog is database in postgres
+            $sql = 'SELECT column_name AS "column_name",data_type AS "data_type" FROM information_schema.columns ';
+            $sql .= 'WHERE table_catalog=:database AND table_name=:table';
+        }
+
+        $rows = DB::select($sql, ['database' => $database, 'table' => $table]);
         $fieldType = null;
-        foreach ($colsType as $col) {
-            if ($field == $col->Field) {
-                $fieldType = $col->Type;
+        $fieldLength = 20;
+
+        foreach ($rows as $col) {
+            if ($field == $col->column_name) {
+                $fieldType = $col->data_type;
+                //column_type not available in postgres SQL
+                //mysql 8 optional display width for int,bigint numeric field
+
+                if ($driver == 'mysql') {
+                    //example: column_type int(11) to 11
+                    preg_match("/(?<=\().+?(?=\))/", $col->column_type, $tblFieldLength);
+                    if (count($tblFieldLength)) {
+                        $fieldLength = $tblFieldLength[0];
+                    }
+                }
+
                 break;
             }
         }
 
-        if ($fieldType == null) throw new Exception("$field not found in $table table");
-        return $fieldType;
+        if ($fieldType == null) {
+            throw new Exception("$field not found in $table table");
+        }
+        return ['type' => $fieldType, 'length' => $fieldLength];
     }
 
+    /**
+     * Main function to generate ID
+     *
+     * @param array $configArr configuration to generate ID
+     *
+     * @throws Exception if id generation config invalid
+     * @return string|integer
+     *
+     * @since 1.0.0
+     */
     public static function generate($configArr)
     {
         if (!array_key_exists('table', $configArr) || $configArr['table'] == '') {
@@ -35,30 +90,33 @@ class IdGenerator
         }
 
         if (array_key_exists('where', $configArr)) {
-            if (is_string($configArr['where']))
+            if (is_string($configArr['where'])) {
                 throw new Exception('where clause must be an array, you provided string');
-            if (!count($configArr['where']))
+            }
+            if (!count($configArr['where'])) {
                 throw new Exception('where clause must need at least an array');
+            }
         }
+        $connection = array_key_exists('connection', $configArr) ? $configArr['connection'] : config('database.default');
 
         $table = $configArr['table'];
         $field = array_key_exists('field', $configArr) ? $configArr['field'] : 'id';
         $prefix = $configArr['prefix'];
-        $resetOnPrefixChange = array_key_exists('reset_on_prefix_change', $configArr) ? $configArr['reset_on_prefix_change'] : false;
+        $resetOnPrefixChange =  array_key_exists('reset_on_prefix_change', $configArr)
+                                ? $configArr['reset_on_prefix_change']
+                                : false;
         $length = $configArr['length'];
 
-        $fieldType = (new self)->getFieldType($table, $field);
-        preg_match("/^([\w\-]+)/", $fieldType, $type);
-        $tableFieldType = $type[0];
-        preg_match("/(?<=\().+?(?=\))/", $fieldType, $tblFieldLength);
-        $tableFieldLength = $tblFieldLength[0];
+        $fieldInfo = (new self)->getFieldType($table, $field, $connection);
+        $tableFieldType = $fieldInfo['type'];
+        $tableFieldLength = $fieldInfo['length'];
 
-        if (in_array($tableFieldType, ['int', 'bigint', 'numeric']) && !is_numeric($prefix)) {
-            throw new Exception("table field type is $tableFieldType but prefix is string");
+        if (in_array($tableFieldType, ['int', 'integer', 'bigint', 'numeric']) && !is_numeric($prefix)) {
+            throw new Exception("$field field type is $tableFieldType but prefix is string");
         }
 
         if ($length > $tableFieldLength) {
-            throw new Exception('ID length is bigger then field length');
+            throw new Exception('Generated ID length is bigger then table field length');
         }
 
         $prefixLength = strlen($configArr['prefix']);
@@ -75,21 +133,23 @@ class IdGenerator
 
 
         $totalQuery = sprintf("SELECT count(%s) total FROM %s %s", $field, $configArr['table'], $whereString);
-        $total = DB::select($totalQuery);
+        // Add connection when are multi tenant
+        $total = DB::connection($connection)->select(trim($totalQuery));
 
         if ($total[0]->total) {
             if ($resetOnPrefixChange) {
-                $maxQuery = sprintf("SELECT MAX(%s) maxId from %s WHERE %s like %s", $field, $table, $field, "'" . $prefix . "%'");
+                $maxIdSql = "SELECT MAX(%s) AS maxid FROM %s WHERE %s LIKE %s";
+                $maxQuery = sprintf($maxIdSql, $field, $table, $field, "'" . $prefix . "%'");
             } else {
-                $maxQuery = sprintf("SELECT MAX(%s) maxId from %s", $field, $table);
+                // Change to select any folio LIKE prefix, this not import length folio
+                $maxQuery = sprintf("SELECT MAX(%s) AS maxid FROM %s WHERE folio LIKE '%s%%'", $field, $table, $prefix);
             }
 
             $queryResult = DB::select($maxQuery);
-            $maxFullId = $queryResult[0]->maxId;
+            $maxFullId = $queryResult[0]->maxid;
 
             $maxId = substr($maxFullId, $prefixLength, $idLength);
-            return $prefix . str_pad($maxId + 1, $idLength, '0', STR_PAD_LEFT);
-
+            return $prefix . str_pad((int)$maxId + 1, $idLength, '0', STR_PAD_LEFT);
         } else {
             return $prefix . str_pad(1, $idLength, '0', STR_PAD_LEFT);
         }
